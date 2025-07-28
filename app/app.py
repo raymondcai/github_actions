9ef3dab1a97e90ca85e688a9c0988578fd5f2315
Raymond Cai <raymondcai@gmail.com>
2025-07-27 22:39:21 -0700

    update to v0.2

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Docker and GitHub Actions! v0.1"
+    return "Hello from Docker and GitHub Actions! v0.2"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
